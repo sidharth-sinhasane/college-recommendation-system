d0ad0b4ee92b5bbfa23bf94e6860556d76ccedb2
sidharth_sinhasane <sidharth.sinhasane@gmail.com>
2024-10-04 12:14:32 +0530

    login logic added

---
+++ client/auth/signup.js
@@ -0,0 +1,15 @@
+let email=""
+let password=""
+let sign_up=false
+
+function signup(){
+    email=document.getElementById("email")
+    password=document.getElementById("password")
+    sign_up=true
+}
+
+function login(){
+    if(!sign_up){
+        
+    }
+}
